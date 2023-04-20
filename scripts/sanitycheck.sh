7c55fa35a5f2c980256de08332649b9a9a80196b
Louis Gevers <louis.gevers@gmail.com>
2023-04-20 22:11:20 +0200

    Add OpenPifPaf to environment with prediction test

---
+++ scripts/sanitycheck.sh
@@ -0,0 +1,6 @@
+#############################################################
+# Script to check whether OpenPifPaf is correctly installed #
+#############################################################
+
+# Take a CrowdPose image and run a prediction, store the example output
+python -m openpifpaf.predict crowdpose/images/110000.jpg --image-output out/sanity-check-image.jpg --json-output out/sanity-check-annotation.json
