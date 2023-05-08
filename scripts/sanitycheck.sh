aad02bb99c506189dd95cdf5c457bc9480e84a44
Louis Gevers <louis.gevers@gmail.com>
2023-05-08 15:50:44 +0200

    Fix sanitycheck script

--- scripts/sanitycheck.sh
+++ scripts/sanitycheck.sh
@@ -3,4 +3,4 @@
 #############################################################
 
 # Take a CrowdPose image and run a prediction, store the example output
-python -m openpifpaf.predict crowdpose/images/110000.jpg --image-output out/sanity-check-image.jpg --json-output out/sanity-check-annotation.json
+python -m openpifpaf.predict data-crowdpose/images/110000.jpg --image-output out/sanity-check-image.jpg --json-output out/sanity-check-annotation.json
