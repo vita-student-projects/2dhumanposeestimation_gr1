d4146df28e97195658d23304977e29ef4713f844
Louis Gevers <louis.gevers@gmail.com>
2023-05-29 20:05:08 +0200

    Add checkpoints download script

---
+++ checkpoints/checkpoints.sh
@@ -0,0 +1,8 @@
+# Pruned resnet checkpoint
+gdown https://drive.google.com/uc?id=11sfqNvXoxDZxm4Hj2iWC27yp6PZzknYg
+# Pruned shufflenet checkpoint (first iteration)
+gdown https://drive.google.com/uc?id=1eBSyuzpHm0IK7icSKeaaiF40axIJduSg
+# Pruned shufflenet checkpoint (second iteration)
+gdown https://drive.google.com/uc?id=1VsP7glaOsGTkyutTIaghgR18_p3g2-q0
+# Swin checkpoint
+gdown https://drive.google.com/uc?id=1ziibZNgk0bS0UNM9op4zGA170YTvqhbx
